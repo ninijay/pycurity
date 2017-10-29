a787263d88ebee957f6936474fe2ed2e7defc03e
root <ninijay93@gmail.com>
2017-10-29 18:46:04 +0100

    fixed port scanner

--- scanners/port_scanner.py
+++ scanners/port_scanner.py
@@ -1,13 +1,22 @@
 import optparse
 from socket import *
+from threading import *
+screenLock = Semaphore(value=1)
 def connScan(tgtHost, tgtPort):
     try:
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
+        connSkt.send("hackerman")
+        results = connSkt.recv(1024)
+        screenLock.acquire()
         print "[+] %d/tcp open" % (tgtPort)
-        connSkt.close
+        print "[+] " + str(results)
     except:
+        screenLock.acquire()
         print "[-] %d/tcp closed" % tgtPort
+    finally:
+        screenLock.release()
+        connSkt.close()
 def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
@@ -21,8 +30,8 @@ def portScan(tgtHost, tgtPorts):
         print "\n[+] Scan Results for: " + tgtIP
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
-        print "Scanning port " + tgtPort
-        connScan(tgtHost, int(tgtPort.strip()))
+       t = Thread(target=connScan, args=(tgtHost, int(tgtPort.strip())))
+       t.start()
 def main():
     parser = optparse.OptionParser(('usage %prog -H <target host> -p <target port(s) separated by space>'))
     parser.add_option("-H", dest="tgtHost", type="string", help="specify tarhet host")
